11d166f4f5434f1c9a014f8bdfadc8c0da45d12a
WashieMugo <washi.mugo@gmail.com>
2021-12-18 00:24:18 +0300

    minor update on sets and lists

--- 2practice.py
+++ 2practice.py
@@ -30,6 +30,7 @@ def dict_func():
         print(bio)
 
 # sets are used to store unique values of a collection
+# sets are unordered. thus they do not have indices
 
 
 def sets_func():
@@ -46,9 +47,57 @@ def sets_func():
 
     print("Iteration using for loop")
     for code in postcodes:
-        print(code)
+        print(f"option: {code}")
+
+    print("Remove using  .remove('set_value') ")
+    print(postcodes)
+    postcodes.remove("Mrnga")
+    print(postcodes)
+
+    print("Avoid Errors by first using in to check if element exists before removing:")
+    if "Nbi" in postcodes:
+        postcodes.remove("Nbi")
+    print(postcodes)
+
+    print("get length usin: len(set_name)")
+    print(len(postcodes))
+
+    print("Check if set is a subset of another set using: # set_name.issubset(subset_name)")
+    postcodes2 = {"Ksmu", "Mbsa"}
+    postcodes3 = {"Nbi", "Nkuru", 'Mbsa'}
+    print(f"P1:{postcodes} \nP2:{postcodes2} \nP3:{postcodes3}")
+    p2sub = postcodes2.issubset(postcodes)
+    p3sub = postcodes3.issubset(postcodes)
+    print(f"P2 sub P1: {p2sub} \nP3 sub P1: {p3sub} ")
+
+    print("Check if a set is Superset to a subset using: # .issuperset(subset_name)")
+    p1sup2 = postcodes.issuperset(postcodes2)
+    p1sup3 = postcodes.issuperset(postcodes3)
+    print(f"P1 super P2: {p2sub} \nP1 super P3: {p3sub} ")
+
+    print("Join sets using:egs # postcodes2.union(postcodes3)")
+    print(postcodes2.union(postcodes3))
+
+    print("Check elements present in both sets using : # intersection() ")
+    print(postcodes3.intersection(postcodes2))
+
+    print("Check elements different in sets using : # difference()")
+    print(postcodes3.difference(postcodes2))
+
+# Lists
+
+
+def lists_func():
+    grocery_list = ["Brocolli", "Cereal", "milk", "Brocolli"]
+    print(grocery_list)
+
+    print("transform to set using set(list_name)")
+    print(set(grocery_list))
+    grocery_set = set(grocery_list)
 
 
 # --------- Function calls -----------
+
 # dict_func()
 sets_func()
+# lists_func()
---
+++ readme.md
@@ -0,0 +1,20 @@
+# DICTIONARIES
+
+=> Structure
+dict_name = {  
+ "key": value,
+"key2": "value"
+}
+=>Operations:
+dict_name["key3"] = Value
+dict_name.pop("Key")
+"key" in dict_name
+
+# SETS
+
+=> Structure:
+sets_name = {val1,val2,val3}
+
+=> Operations
+sets_name.add(value)
+sets_name.remove(val)
