56a3f4656a2ea579e51fd394eb991048b7dbc91d
WashieMugo <washi.mugo@gmail.com>
2021-12-20 18:25:29 +0300

    Encapsulation

--- Others/classes/oop4.py
+++ Others/classes/oop4.py
@@ -1,1 +1,22 @@
 # Abstracting obejcts
+class Car:
+    def __init__(self):
+        self.on = False
+
+    def injectFuel(self):
+        print("Spraying Fuel..")
+
+    def igniteFuel(self):
+        print("Igniting Fuel")
+
+    def startUp(self):
+        self.on = True
+        count = 0
+        while self.on and count <= 10:
+            self.injectFuel()
+            self.igniteFuel()
+            count += 1
+
+
+myCar = Car()
+myCar.startUp() # the core method. no need to know what the oher methods do
